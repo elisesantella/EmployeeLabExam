7a81b674649715a0946d4e3462fc7797961b716a
G00387170 <g00387170@atu.ie>
2022-12-07 09:32:54 +0000

    Creating Constructor, methods and tests for name variable

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -1,4 +1,18 @@
 package ie.atu;
 
 public class Employee {
+    private String name;
+
+    public Employee(String nameSize){
+        if(nameSize.length() >= 5 && nameSize.length() <= 22){
+            this.name = nameSize;
+        }
+        else{
+            throw new IllegalArgumentException("Name is invalid! Must be between 5 -22 characters");
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
 }
--- src/test/java/ie/atu/EmployeeTest.java
+++ src/test/java/ie/atu/EmployeeTest.java
@@ -2,13 +2,31 @@ package ie.atu;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    void testEmployeeNameSuccess(){
+        Employee newEmployee = new Employee("elise");
+        assertEquals("elise", newEmployee.getName());
+    }
+
+    @Test
+    void testEmployeeNameMinFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Employee ("el");});
+        assertEquals("Name is invalid! Must be between 5 -22 characters", exMessage.getMessage());
+    }
+
+    @Test
+    void testEmployeeNameMaxFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Employee ("elisereneelouisesantella");});
+        assertEquals("Name is invalid! Must be between 5 -22 characters", exMessage.getMessage());
     }
 
     @AfterEach
