4ca4f9e70090142db4d50034d6a479e76a2bdd69
G00387170 <g00387170@atu.ie>
2022-12-07 10:06:20 +0000

    Adding PPS class and test

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -2,17 +2,29 @@ package ie.atu;
 
 public class Employee {
     private String name;
+    private String pps;
 
-    public Employee(String nameSize){
+    public Employee(String nameSize, String ppsSize){
         if(nameSize.length() >= 5 && nameSize.length() <= 22){
             this.name = nameSize;
         }
         else{
             throw new IllegalArgumentException("Name is invalid! Must be between 5 -22 characters");
         }
+
+        if(ppsSize.length() == 6){
+            this.pps = ppsSize;
+        }
+        else{
+            throw new IllegalArgumentException("PPS ID invalid! Must be 6 characters long");
+        }
     }
 
     public String getName(){
         return name;
+    }
+
+    public String getPPS(){
+        return pps;
     }
 }
